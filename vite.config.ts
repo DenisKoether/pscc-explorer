784bdda3643eddbdd4d7d63a74adeb8a06c482ca
Denis Köther <denis.koether@dkfz-heidelberg.de>
2025-05-08 15:05:10 +0200

    chore: updated host

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	server: {
-		allowedHosts: ['bridgehead.dev.dzif.de']
+		allowedHosts: ['demo.pscc.samply.de']
 	},
 	plugins: [sveltekit()],
 	test: {
